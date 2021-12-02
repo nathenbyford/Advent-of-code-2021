9cff79a496c852a212de694a3ed71af920ede197
nathenbyford <Nathenbyford@gmail.com>
2021-12-01 21:21:09 -0600

    Day 1

---
+++ Day_01.R
@@ -0,0 +1,18 @@
+##########################
+## Nathen Byford
+## Dec. 1, 2021
+##########################
+library("tidyverse")
+
+dat <- c(199, 200, 208, 210, 200, 207, 240, 269, 260, 263)
+
+count_increases <- function(input, lag = 1) {
+    count = sum(diff(input, lag = lag) > 0)
+    return(count)
+}
+
+count_increases(dat, 3)
+
+large_dat <- read_lines(".//data//day_01.txt")
+
+large_dat |> as.numeric() |> count_increases(lag = 3)
